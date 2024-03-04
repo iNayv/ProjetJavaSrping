6315f8c041ecf9695738ea70c351d8168f78ebf1
Maxime CLAIN <mclain.cda@FORMATION.LOCAL>
2024-03-04 13:26:50 +0400

    first commit

---
+++ src/main/java/com/projet/ProjetJavaSpring/ProjetJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projet.ProjetJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/ProjetJavaSpring/ProjetJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.ProjetJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
